044486dc89795e41e115d72eb4301bf665b36cf6
AndreiaOliveira7 <andreia_deoliveira@ymail.com>
2023-05-14 21:48:15 -0300

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: "/dev-movies",
+  base: "/dev-movies",
   plugins: [react()],
 });
